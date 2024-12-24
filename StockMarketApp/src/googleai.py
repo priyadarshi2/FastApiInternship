0dd639b7d9b877cc37a86032b11fc7a55530db10
Priyadarshi_Shakya <shakyapriyadarshi@gmail.com>
2024-12-24 10:27:58 +0530

    stored the api in .env file

--- StockMarketApp/src/googleai.py
+++ StockMarketApp/src/googleai.py
@@ -1,7 +1,11 @@
 import google.generativeai as genai
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def gooogleAI(symb : str, txt : str):
-    genai.configure(api_key="AIzaSyDJVMDPSsF7BGBIrd-RAgXoY4x3U55RdDk")
+    genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(f"answer me yes or no about this stock {symb} considering this meta description about the stock :\n {txt}")
     return str(response.text)
